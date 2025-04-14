a56fa69154d6814c72fad0aa4c16695caed10b1b
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-14 15:53:54 -0400

    Define a Terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,15 @@
-terraform {
+ terraform {
   backend "s3" {
     encrypt = true
   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   default_tags {
     tags = local.default_tags
@@ -18,35 +17,22 @@ provider "aws" {
   region  = var.aws_region
 }
 
-# Data sources
 data "aws_caller_identity" "current" {}
 
-data "aws_cloudwatch_log_group" "cw_log_group" {
-  name = "/aws/batch/job/${var.prefix}-momma/"
-}
-
-data "aws_efs_file_system" "aws_efs_input" {
-  creation_token = "${var.prefix}-input"
-}
-
-data "aws_efs_file_system" "aws_efs_flpe" {
-  creation_token = "${var.prefix}-flpe"
-}
-
-data "aws_iam_role" "job_role" {
-  name = "${var.prefix}-batch-job-role"
-}
-
-data "aws_iam_role" "exe_role" {
-  name = "${var.prefix}-ecs-exe-task-role"
-}
-
-# Local variables
 locals {
-  account_id = data.aws_caller_identity.current.account_id
+  account_id = sensitive(data.aws_caller_identity.current.account_id)
   default_tags = length(var.default_tags) == 0 ? {
     application : var.app_name,
     environment : lower(var.environment),
     version : var.app_version
   } : var.default_tags
+}
+
+module "confluence-momma" {
+  source            = "./modules/momma"
+  app_name          = var.app_name
+  app_version       = var.app_version
+  aws_region        = var.aws_region
+  environment       = var.environment
+  prefix            = var.prefix
 }
---
+++ terraform/modules/momma/main.tf
@@ -0,0 +1,32 @@
+# Data sources
+data "aws_caller_identity" "current" {}
+
+data "aws_cloudwatch_log_group" "cw_log_group" {
+  name = "/aws/batch/job/${var.prefix}-momma/"
+}
+
+data "aws_efs_file_system" "aws_efs_input" {
+  creation_token = "${var.prefix}-input"
+}
+
+data "aws_efs_file_system" "aws_efs_flpe" {
+  creation_token = "${var.prefix}-flpe"
+}
+
+data "aws_iam_role" "job_role" {
+  name = "${var.prefix}-batch-job-role"
+}
+
+data "aws_iam_role" "exe_role" {
+  name = "${var.prefix}-ecs-exe-task-role"
+}
+
+# Local variables
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  default_tags = length(var.default_tags) == 0 ? {
+    application : var.app_name,
+    environment : lower(var.environment),
+    version : var.app_version
+  } : var.default_tags
+}
